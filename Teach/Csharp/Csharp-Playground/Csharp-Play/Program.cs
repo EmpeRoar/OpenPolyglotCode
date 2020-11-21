0eb223c3e5acb335049f645a1dc7244329c48d2f
Julius Bacosa <juliusbacosa@gmail.com>
2020-11-21 13:00:13 +0800

    .

--- Teach/Csharp/Csharp-Playground/Csharp-Play/Program.cs
+++ Teach/Csharp/Csharp-Playground/Csharp-Play/Program.cs
@@ -1,22 +1,35 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Csharp_Play
 {
+    public class Person
+    {
+        public string Name { get; set; }
+    }
+
     public class Program
     {
         private static async Task Main(string[] args)
         {
-
-            IEnumerable<string> strList = null;
-
-            strList.ToList().ForEach(x =>
+            var json = "{\"Name\":\"JBX\"}";
+            dynamic x = JsonConvert.DeserializeObject(json);
+            // var x = JsonConvert.DeserializeObject<Person>(json);
+            try
             {
-                Console.WriteLine($"{x}");
-            });
+                Console.WriteLine(x.Name);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
             Console.ReadLine();
         }
+
     }
 }
