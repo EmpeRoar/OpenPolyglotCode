72c05f80249631340eb625edd515b284e81b9f54
Julius Bacosa <juliusbacosa@gmail.com>
2020-11-22 11:41:14 +0800

    sign is added

---
+++ JquerySketches/carousel/js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    console.log('its working!');
+    
+    $('#left').on('click', function(){
+        console.log('left is clicked');
+    });
+    $('#right').on('click', function(){
+        console.log('right is clicked');
+    });
+});
